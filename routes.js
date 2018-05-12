443f73c9a3a8a658d3399e213f058127694a2c80
unknown <anthonyj.manenti@gmail.com>
2018-05-12 12:58:14 -0400

    added corrections to requirements between pages.

--- routes.js
+++ routes.js
@@ -1,7 +1,8 @@
 
 var bodyParser = require("body-parser");
 var path = require("path");
-
+var express = require('express')
+var app = express()
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 var express = require('express')
-var routes = require('./routes.js')
-
-
 var app = express()
+var routes = require('./routes.js');
 
- 
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
  
+
+var tables = {
+
+name: '',
+phoneNumber: '',
+email: '',
+uniqueID: ''
+
+}
+
+
 app.listen(3000)
 console.log('listening on port 3000')
