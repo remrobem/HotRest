596e909bf5f6c6482d221b1a6c82827cd68db58d
Rob Martin <rob.martin.5758@gmail.com>
2018-05-12 11:27:08 -0400

    update

--- routes.js
+++ routes.js
@@ -1,4 +1,12 @@
 
+var bodyParser = require("body-parser");
+var path = require("path");
+
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
   
   // Routes
   // =============================================================
